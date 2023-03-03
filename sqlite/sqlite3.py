1e075ff046b9a4b4b816d21746bd12af4053f910
untletch <untletch@gmail.com>
2023-03-03 23:48:04 +0300

    added script to download latest sqlite3

---
+++ sqlite/sqlite3.py
@@ -0,0 +1,53 @@
+import hashlib
+import os
+import pathlib
+from datetime import datetime
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def scrape_downloads_page():
+    url = "https://www.sqlite.org/download.html"
+    resp = requests.get(url)
+    if resp.status_code != 200:
+        raise Exception(f"error: response status code: {resp.status_code}")
+    soup = BeautifulSoup(resp.content, "html.parser")
+    filename_tag = soup.find(id="a5")
+    filename = soup.find(id="a5").text.strip()
+    file_hash = filename_tag.parent.parent.find("small").text.split()[1].strip(")")
+    return file_hash, filename
+
+
+def download_sqlite_binary():
+    _, filename = scrape_downloads_page()
+    filepath = os.path.join(pathlib.Path.home(), "Downloads", filename)
+    url = f"https://www.sqlite.org/{datetime.now().year}/{filename}"
+    resp = requests.get(url)
+    if resp.status_code != 200:
+        raise Exception(f"error: response status code: {resp.status_code}")
+    binary = resp.content
+    with open(filepath, "wb") as f:
+        f.write(binary)
+    return filepath
+
+
+def compare_hash():
+    BLOCK_SIZE = 4 * 1024
+    saved_filename = download_sqlite_binary()
+    expected, _ = scrape_downloads_page()
+
+    file_hash = hashlib.sha3_256()
+    with open(saved_filename, "rb") as f:
+        fb = f.read(BLOCK_SIZE)
+        while len(fb) > 0:
+            file_hash.update(fb)
+            fb = f.read(BLOCK_SIZE)
+
+    digest = file_hash.hexdigest()
+    assert expected == digest, f"expected {expected} got {digest}"
+
+
+if __name__ == "__main__":
+    download_sqlite_binary()
+    compare_hash()
